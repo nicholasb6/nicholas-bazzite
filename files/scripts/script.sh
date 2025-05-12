81cf0aa02b2455875d0e430ea2acad35ad31f47a
nicholas <127063443+nicholasb6@users.noreply.github.com>
2025-05-11 23:10:21 -0500

    trying a different method to disable wifi powersave

--- files/scripts/script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-# Your code goes here.
-printf "[connection]\nwifi.powersave = 2" | sudo tee /etc/NetworkManager/conf.d/wifi-powersave-off.conf
